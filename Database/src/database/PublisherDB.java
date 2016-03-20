ababb14358dbcac339e437f511ca9757beb3b13e
onemorelife <bkhan9279@gmail.com>
2016-03-20 00:13:47 -0400

    Database methods
    Added connection method, need to add methods to  update, add, delete

---
+++ Database/src/database/PublisherDB.java
@@ -0,0 +1,38 @@
+package database;
+
+import java.util.*;
+import java.sql.*;
+
+/**
+ *
+ * @author Badruddoza Khan
+ */
+public class PublisherDB 
+{
+private Connection getConnection()
+    {
+        Connection connection = null;
+        try
+        {
+            // set the home directory for Derby
+            String dbDirectory = "/database/java/db"; //NOTE:Will probably need adjusting                                                                    
+            System.setProperty("derby.system.home", dbDirectory);
+
+            // set the db url, username, and password
+            String url = "jdbc:derby:PublisherDB";
+            String username = "";
+            String password = "";
+
+            connection = DriverManager.getConnection(url, username, password);
+			return connection;
+        }
+        catch(SQLException e)
+        {
+            System.err.println(e);
+			return null;
+        }
+    }
+
+    //TODO: methods for getting, adding, deleting 
+    
+}
