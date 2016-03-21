93c426c6cfb9396376edf3cca91b545d5b88d9f8
atkachenko893 <atkachenko893@KBC502-29.administration.hcc>
2016-03-21 13:54:55 -0400

    Added a generic Record class

---
+++ Database/src/database/Record.java
@@ -0,0 +1,38 @@
+package database;
+
+import java.util.ArrayList;
+
+public class Record 
+{
+    private final ArrayList<String> fieldNames;
+    private ArrayList<Object> fieldValues;
+    
+    /**
+     * The Field value has to have a matching index to the name
+     * IE the object in the third position of FieldValues is the
+     * value located at the third field in the result set
+     * @param fieldNames
+     * @param fieldValues 
+     */
+    public Record(ArrayList<String> fieldNames, ArrayList<Object> fieldValues)
+    {
+        this.fieldNames = fieldNames;
+        this.fieldValues = fieldValues;
+    }
+    
+    public void setValAt(String value, int index)
+    {
+        fieldValues.remove(index);
+        fieldValues.add(index, value);
+    }
+    
+    public ArrayList<String> getFieldNames()
+    {
+        return fieldNames;
+    }
+    
+    public Object getValAt(int index)
+    {
+        return fieldValues.get(index);
+    }
+}
